f91a0df7e8ccc9c84602ef8711184083c4df113f
sergsenta1995 <sergsenta1995@gmail.com>
2016-06-28 18:01:21 +0400

    add SURFMatcher() SURFMatcher.cpp

--- LogoRecognitionAIFIL/LogoRecognitionAIFIL/SURFMatcher.cpp
+++ LogoRecognitionAIFIL/LogoRecognitionAIFIL/SURFMatcher.cpp
@@ -1,2 +1,9 @@
 #include "SURFMatcher.h"
 
+SURFMatcher::SURFMatcher(Mat imagePattern_, Mat imageScene_) :
+	IMatcher(imagePattern_, imageScene_)
+	
+{
+	m_matchingThresholdValue = 1500;
+	m_detector = new SurfFeatureDetector();
+}
