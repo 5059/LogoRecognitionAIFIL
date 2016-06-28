eb79c08f94e010dd0510be4ebaac3c5f6bc17fca
sergsenta1995 <sergsenta1995@gmail.com>
2016-06-28 18:02:21 +0400

    add DetectKeyPoints() SURFMatcher.cpp

--- LogoRecognitionAIFIL/LogoRecognitionAIFIL/SURFMatcher.cpp
+++ LogoRecognitionAIFIL/LogoRecognitionAIFIL/SURFMatcher.cpp
@@ -7,3 +7,15 @@ SURFMatcher::SURFMatcher(Mat imagePattern_, Mat imageScene_) :
 	m_matchingThresholdValue = 1500;
 	m_detector = new SurfFeatureDetector();
 }
+
+
+SURFMatcher::~SURFMatcher()
+{	
+}
+
+
+void SURFMatcher::DetectKeyPoints()
+{
+	m_detector->detect(m_imagePattern, m_keypointsImagePattern);
+	m_detector->detect(m_imageScene, m_keypointsImageScene);
+}
