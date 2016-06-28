4b18ff1268dfb9ea5331718ff2febe7ec86762c0
sergsenta1995 <sergsenta1995@gmail.com>
2016-06-28 17:58:29 +0400

    correct IMacher

--- LogoRecognitionAIFIL/LogoRecognitionAIFIL/IMatcher.h
+++ LogoRecognitionAIFIL/LogoRecognitionAIFIL/IMatcher.h
@@ -6,17 +6,27 @@
 
 class IMatcher {
 protected:
-	int                  m_matchingThresholdValue;
-	Feature2D           *m_detector;
-	DescriptorExtractor *m_extractor;
-	DescriptorMatcher   *m_matcher;
+	int                   m_matchingThresholdValue;
+	Feature2D            *m_detector;
+	DescriptorExtractor  *m_extractor;
+	DescriptorMatcher    *m_matcher;
+	Mat                   m_imagePattern;
+	Mat                   m_imageScene;
+	Mat                   m_descriptorsImagePattern;
+	Mat                   m_descriptorsImageScene;
+	std::vector<KeyPoint> m_keypointsImagePattern;
+	std::vector<KeyPoint> m_keypointsImageScene;
+	std::vector<DMatch>   m_correctMatches;
+
 	virtual void ComputeDescriptors() = 0;
 	virtual void DetectKeyPoints()    = 0;
 public:
-	IMatcher(){};
+	IMatcher(Mat imagePattern_, Mat imageScene_) :
+		m_imagePattern(imagePattern_),
+		m_imageScene(imageScene_) {};
 	virtual ~IMatcher(){};
 
-	virtual void Match(Image *pattern_image_, Image *images_) = 0;	
+	virtual void Match() = 0;	
 };
 
 #endif
